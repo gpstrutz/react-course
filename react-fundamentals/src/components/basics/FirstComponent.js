d148284a205da62e48779db6c34a00d62e6d1021
gpstrutz <giovannistrutz97@gmail.com>
2021-01-25 21:02:50 -0300

    Created the components folder and the first simple React component

---
+++ react-fundamentals/src/components/basics/FirstComponent.js
@@ -0,0 +1,3 @@
+export default function firstComponent() {
+    return 'My first component!'
+}
--- react-fundamentals/src/index.js
+++ react-fundamentals/src/index.js
@@ -2,24 +2,16 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import './index.css'
 
+import First from './components/basics/FirstComponent'
+
 const element = document.getElementById('root')
 
 
 // Using JSX sintax
 ReactDOM.render (
     <div>
-        <strong>Hello React!</strong>
+        <strong>
+            <First></First>
+        </strong>
     </div>, element
 )
-
-// We can interpolate variables on JSX code
-
-/* 
-    const greeting = <strong>Hello World!</strong>
-
-    ReactDOM.render (
-        <div>
-            { greeting }
-        </div>, element
-    )
-*/
